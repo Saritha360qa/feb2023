e60b84bc6e63c094b10e3ee120e20c19aa147061
saritha360qa <saritha.muthavarapu@gmail.com>
2023-02-23 18:57:55 +1300

    login tests added

---
+++ Automation1/Automation1/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+// open chrome browser 
+
+IWebDriver Driver = new ChromeDriver();
+
+// lunch turnup portal
+Driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+// Identify the username textbox and enter valid username
+IWebElement UsernameTextbox = Driver. FindElement(By.Id("UserName"));
+UsernameTextbox.SendKeys ("Hari");
+
+// Identify the password textbox and enter valid password
+IWebElement PasswordTextbox = Driver. FindElement(By.Id("Password"));
+PasswordTextbox.SendKeys ("123123");
+
+// Identify the login button and click on it
+IWebElement LoginButton = Driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+LoginButton.Click();
